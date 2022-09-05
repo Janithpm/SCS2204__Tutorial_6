da3a1379f0e44a2efc40f9994853b18d1dea3652
Janith Madarasinghe <janithpm9991@gmail.com>
2022-09-05 23:32:08 +0530

    updated main.scala

--- main.scala
+++ main.scala
@@ -5,9 +5,9 @@ object Main {
     def D(c: Char, key: Int, a: String) = a((a.indexOf(c.toUpper) - key) % a.size)
 
     def cipher(algo: (Char, Int, String) => Char, s: String, key: Int, a: String) = s.map(algo(_, key, a))
-    def p(s: String, c: Int) = println((s.indexOf(c) + 5) % s.size)
+
     def main(args: Array[String]) = {
-        val ct = cipher(E, "Hello World", 5, alphabet)
+        val ct = cipher(E, "HelloWorld", 5, alphabet)
         println(ct)
         val pt = cipher(D, ct, 5, alphabet)
         println(pt)
