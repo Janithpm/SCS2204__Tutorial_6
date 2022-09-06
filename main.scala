e8ae52fd7e4abb01d7e78de30852554736203e1e
Janith Madarasinghe <janithpm9991@gmail.com>
2022-09-06 06:49:55 +0530

    minus index bug fixed

--- main.scala
+++ main.scala
@@ -1,14 +1,24 @@
 object Main {
-    val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ .,"
+    
+    def shift(dir: Int, idx: Int, key: Int, asize: Int) = {
+        var temp = (idx + dir * key) % asize
+        if(temp < 0) temp = temp + asize
+        temp 
+    }
+    
+    def E(c: Char, key: Int, a: String) = a(shift(1, a.indexOf(c.toUpper), 5, a.size))
 
-    def E(c: Char, key: Int, a: String) = a((a.indexOf(c.toUpper) + key) % a.size)
-    def D(c: Char, key: Int, a: String) = a((a.indexOf(c.toUpper) - key) % a.size)
+    def D(c: Char, key: Int, a: String) = a(shift(-1, a.indexOf(c.toUpper), 5, a.size))
 
     def cipher(algo: (Char, Int, String) => Char, s: String, key: Int, a: String) = s.map(algo(_, key, a))
 
     def main(args: Array[String]) = {
-        val ct = cipher(E, "HelloWorld", 5, alphabet)
+        val text = "Hello, Functional Programming."
+
+        val ct = cipher(E, text, 5, alphabet)
         println(ct)
+
         val pt = cipher(D, ct, 5, alphabet)
         println(pt)
         
